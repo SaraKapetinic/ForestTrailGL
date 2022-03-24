ea97cbecebc079bce86b6e1326fd7c996f59d06f
Predrag Draganovic <predrag.draganovic@gmail.com>
2022-03-24 19:10:37 +0100

    Refactoring of ImGui code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,3 @@
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-
 #include <iostream>
 #include "glad/include/glad/glad.h"
 #include <GLFW/glfw3.h>
@@ -13,91 +9,24 @@
 #include "Camera.h"
 #include "Terrain.h"
 #include "SkyBox.h"
+#include "GUI.h"
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
-
-glm::vec3 lightPos (100.0f, 100.0f, 100.0f);
-
-struct ProgramState{
-    bool ImguiEnable= false;
-    void LoadFromDisk(std::string path);
-    void SaveToDisk(std::string path) ;
-    Camera camera;
-    glm::vec3 lightColor;
-    glm::vec3 lightColor1;
-    bool EnableMouseMovement = true;
-    float linear = 0.09;
-    float quadratic = 0.032;
-    bool enableAntialiasing = false;
-    ProgramState()
-    :camera(glm::vec3(0.0f,4.0f,35.5f)),lightColor(glm::vec3(1.0f,1.0f,1.0f))
-    ,lightColor1(glm::vec3(1.0f,1.0f,1.0f)){}
-
-};
-
-void ProgramState::SaveToDisk(std::string path) {
-    std::ofstream out(path);
-
-    out<<ImguiEnable<<'\n'
-    <<camera.Position.x<<'\n'
-    <<camera.Position.y<<'\n'
-    <<camera.Position.z<<'\n'
-    <<camera.Front.x<<'\n'
-    <<camera.Front.y<<'\n'
-    <<camera.Front.z<<'\n'
-    <<camera.Pitch<<'\n'
-    <<camera.Yaw<<'\n'
-    <<lightColor.r<<'\n'
-    <<lightColor.g<<'\n'
-    <<lightColor.b<<'\n'
-    <<lightColor1.r<<'\n'
-    <<lightColor1.g<<'\n'
-    <<lightColor1.b<<'\n'
-    <<linear<<'\n'
-    <<quadratic;
-}
-
-void ProgramState::LoadFromDisk(std::string path) {
-    std::ifstream in(path);
-
-    if(in){
-        in>>ImguiEnable
-        >>camera.Position.x
-        >>camera.Position.y
-        >>camera.Position.z
-        >>camera.Front.x
-        >>camera.Front.y
-        >>camera.Front.z
-        >>camera.Pitch
-        >>camera.Yaw
-        >>lightColor.r
-        >>lightColor.g
-        >>lightColor.b
-        >>lightColor1.r
-        >>lightColor1.g
-        >>lightColor1.b
-        >>linear
-        >>quadratic;
-    }
-}
+ProgramState ps;
 
 glm::vec3 bulbPos[] = {
         glm::vec3(-5.0f, 4.2f, -6.75f),
         glm::vec3(8.03f, 4.2f, 7.75f)
 };
-
-ProgramState* programState;
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
+void mouse_callback(GLFWwindow *window, double xposIn, double yposIn);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
 void key_callback(GLFWwindow* window,int key,int scancode,int action,int mods);
-void DrawImgui(ProgramState* programState);
 
 
 int main() {
@@ -127,24 +56,12 @@ int main() {
 
     stbi_set_flip_vertically_on_load(true);
     glEnable(GL_DEPTH_TEST);
-    //glEnable(GL_MULTISAMPLE); we can press f or g to enable/disable antialiasing
+
     glEnable(GL_CULL_FACE);
 
-    programState = new ProgramState;
-    programState->LoadFromDisk("../resources/programState.txt");
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    GUI imgui(ps, "../resources/programState.txt");
 
-    if(programState->ImguiEnable){
-        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-    }
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();(void)io;
-
-    ImGui::StyleColorsDark();
-
-    ImGui_ImplGlfw_InitForOpenGL(window,true);
-    ImGui_ImplOpenGL3_Init("#version 330 core");
+    imgui.initImGui(window);
 
     Shader shaderProgram("../resources/shaders/model.vs", "../resources/shaders/model.fs");
     std::string bridgePath = std::filesystem::path("../resources/models/bridge.obj");
@@ -182,23 +99,23 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shaderProgram.use();
-
-        glm::mat4 projection = glm::perspective(glm::radians(programState->camera.Zoom),(float)SCR_WIDTH/(float)SCR_HEIGHT,0.1f,100.0f);
-        glm::mat4 view = programState->camera.GetViewMatrix();
+        ProgramState &programState = imgui.getPs();
+        glm::mat4 projection = glm::perspective(glm::radians(programState.camera.Zoom),(float)SCR_WIDTH/(float)SCR_HEIGHT,0.1f,100.0f);
+        glm::mat4 view = programState.camera.GetViewMatrix();
 
         shaderProgram.setMat4("projection", projection);
         shaderProgram.setMat4("view", view);
-        shaderProgram.setVec3("viewPos",programState->camera.Position);
+        shaderProgram.setVec3("viewPos",programState.camera.Position);
         shaderProgram.setVec3("pointLights[0].position", bulbPos[0]);
-        shaderProgram.setVec3("pointLights[0].color", programState->lightColor);
+        shaderProgram.setVec3("pointLights[0].color", programState.lightColor);
         shaderProgram.setFloat("pointLights[0].constant", 1.0);
-        shaderProgram.setFloat("pointLights[0].linear",programState->linear);
-        shaderProgram.setFloat("pointLights[0].quadratic", programState->quadratic);
+        shaderProgram.setFloat("pointLights[0].linear",programState.linear);
+        shaderProgram.setFloat("pointLights[0].quadratic", programState.quadratic);
         shaderProgram.setVec3("pointLights[1].position", bulbPos[1]);
-        shaderProgram.setVec3("pointLights[1].color", programState->lightColor1);
+        shaderProgram.setVec3("pointLights[1].color", programState.lightColor1);
         shaderProgram.setFloat("pointLights[1].constant", 1.0);
-        shaderProgram.setFloat("pointLights[1].linear",programState->linear);
-        shaderProgram.setFloat("pointLights[1].quadratic", programState->quadratic);
+        shaderProgram.setFloat("pointLights[1].linear",programState.linear);
+        shaderProgram.setFloat("pointLights[1].quadratic", programState.quadratic);
 
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(1.6f,1.5f,0.0f));
@@ -250,42 +167,39 @@ int main() {
         terrainShader.setMat4("projection", projection);
         terrainShader.setMat4("model", model);
 
-        terrainShader.setVec3("viewPos",programState->camera.Position);
+        terrainShader.setVec3("viewPos",programState.camera.Position);
         terrainShader.setVec3("pointLights[0].position", bulbPos[0]);
-        terrainShader.setVec3("pointLights[0].color", programState->lightColor);
+        terrainShader.setVec3("pointLights[0].color", programState.lightColor);
         terrainShader.setFloat("pointLights[0].constant", 1.0);
-        terrainShader.setFloat("pointLights[0].linear",programState->linear);
-        terrainShader.setFloat("pointLights[0].quadratic", programState->quadratic);
+        terrainShader.setFloat("pointLights[0].linear",programState.linear);
+        terrainShader.setFloat("pointLights[0].quadratic", programState.quadratic);
         terrainShader.setVec3("pointLights[1].position", bulbPos[1]);
-        terrainShader.setVec3("pointLights[1].color", programState->lightColor1);
+        terrainShader.setVec3("pointLights[1].color", programState.lightColor1);
         terrainShader.setFloat("pointLights[1].constant", 1.0);
-        terrainShader.setFloat("pointLights[1].linear",programState->linear);
-        terrainShader.setFloat("pointLights[1].quadratic", programState->quadratic);
+        terrainShader.setFloat("pointLights[1].linear",programState.linear);
+        terrainShader.setFloat("pointLights[1].quadratic", programState.quadratic);
 
         terrainModel.Draw(terrainShader);
 
         glDepthFunc(GL_LEQUAL);
         skyBoxShader.use();
         skyBoxShader.setInt("skybox", 0);
-        view = glm::mat4(glm::mat3(programState->camera.GetViewMatrix()));
+        view = glm::mat4(glm::mat3(programState.camera.GetViewMatrix()));
         skyBoxShader.setMat4("view", view);
         skyBoxShader.setMat4("projection", projection);
         skyBox.Draw();
         glDepthFunc(GL_LESS);
 
-        if(programState->ImguiEnable){
-            DrawImgui(programState);
+        if(programState.ImguiEnable){
+            imgui.DrawImgui();
         }
 
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
 
-    programState->SaveToDisk("../resources/programState.txt");
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-    delete programState;
+    imgui.deinitImgui();
+
     glfwTerminate();
     return 0;
 }
@@ -300,26 +214,26 @@ void processInput(GLFWwindow* window){
 
     float speed = 0.05f;
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        if(programState->camera.Position.y > 0 )
-            programState->camera.Position += speed * programState->camera.Front;
+        if(ps.camera.Position.y > 0 )
+            ps.camera.Position += speed * ps.camera.Front;
         else {
-            if(programState->camera.Position.y <= 0)
-                if(programState->camera.Front.y > 0 )
-                    programState->camera.Position += speed * programState->camera.Front;
+            if(ps.camera.Position.y <= 0)
+                if(ps.camera.Front.y > 0 )
+                    ps.camera.Position += speed * ps.camera.Front;
         }
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
 
-        if(programState->camera.Position.y > 0 )
-            programState->camera.Position -= speed * programState->camera.Front;
+        if(ps.camera.Position.y > 0 )
+            ps.camera.Position -= speed * ps.camera.Front;
         else {
-            if(programState->camera.Position.y <= 0)
-                if(programState->camera.Front.y < 0 )
-                    programState->camera.Position -= speed * programState->camera.Front;
+            if(ps.camera.Position.y <= 0)
+                if(ps.camera.Front.y < 0 )
+                    ps.camera.Position -= speed * ps.camera.Front;
         }
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        programState->camera.Position -= glm::normalize(glm::cross(programState->camera.Front, programState->camera.Up)) * speed;
+        ps.camera.Position -= glm::normalize(glm::cross(ps.camera.Front, ps.camera.Up)) * speed;
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        programState->camera.Position += glm::normalize(glm::cross(programState->camera.Front, programState->camera.Up)) * speed;
+        ps.camera.Position += glm::normalize(glm::cross(ps.camera.Front, ps.camera.Up)) * speed;
     if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
         glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
     }
@@ -342,87 +256,20 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
 
     lastX = xpos;
     lastY = ypos;
-    if(programState->EnableMouseMovement){
-        programState->camera.ProcessMouseMovement(xoffset, yoffset);
+    if(ps.EnableMouseMovement){
+        ps.camera.ProcessMouseMovement(xoffset, yoffset);
     }
 }
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-    programState->camera.ProcessMouseScroll(static_cast<float>(yoffset));
+    ps.camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
-void DrawImgui(ProgramState* programState){
 
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-
-    {
-        ImGuiIO io = ImGui::GetIO();
-        float display_width = (float)io.DisplaySize.x;
-        float display_height = (float)io.DisplaySize.y;
-        float pos_x = display_width*0.6;
-        float pos_y = display_height*0.7;
-        //ImGui::SetWindowPos(ImVec2(pos_x,pos_y));
-        ImGui::SetNextWindowPos(ImVec2(pos_x,pos_y),ImGuiCond_FirstUseEver);
-        ImGui::Begin("CGraphics");
-        ImGui::ColorEdit3("Bulb color",(float*)&programState->lightColor);
-        ImGui::ColorEdit3("Bulb color1",(float*)&programState->lightColor1);
-
-        ImGui::Text("Linear intensity");
-        if(ImGui::RadioButton("0.7",&programState->linear)){
-            programState->linear = 0.7;
-        }
-        else if(ImGui::RadioButton("0.22",&programState->linear)){
-            programState->linear = 0.22;
-        }
-        else if(ImGui::RadioButton("0.09",&programState->linear)){
-            programState->linear = 0.09;
-        }
-        ImGui::Text("Quadratic intensity");
-        if(ImGui::RadioButton("1.8",&programState->quadratic)){
-            programState->quadratic = 1.8;
-        }
-        else if(ImGui::RadioButton("0.2",&programState->quadratic)){
-            programState->quadratic = 0.2;
-        }
-        else if(ImGui::RadioButton("0.032",&programState->quadratic)){
-            programState->quadratic = 0.032;
-        }
-
-        ImGui::Checkbox("Enable Antialiasing",&programState->enableAntialiasing);
-        if(programState->enableAntialiasing){
-            glEnable(GL_MULTISAMPLE);
-        }else{
-            glDisable(GL_MULTISAMPLE);
-        }
-        ImGui::End();
-    }
-
-    {
-        ImGuiIO io = ImGui::GetIO();
-        float display_width = (float)io.DisplaySize.x;
-        float display_height = (float)io.DisplaySize.y;
-        float pos_x = display_width*0.6;
-        float pos_y = display_height*0.3;
-        //ImGui::SetWindowPos(ImVec2(pos_x,pos_y));
-        ImGui::SetNextWindowPos(ImVec2(pos_x,pos_y),ImGuiCond_FirstUseEver);
-        ImGui::Begin("Camera");
-        auto &c = programState->camera;
-        ImGui::Text("Camera position: (%f, %f, %f)",c.Position.x,c.Position.y,c.Position.z);
-        ImGui::Text("Camera pitch: %f",c.Pitch);
-        ImGui::Text("Camera yaw: %f",c.Yaw);
-        ImGui::Text("Camera front: (%f, %f, %f)",c.Front.x,c.Front.y,c.Front.z);
-        ImGui::Checkbox("Enable camera movement on mouse",&programState->EnableMouseMovement);
-        ImGui::End();
-    }
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-}
 void key_callback(GLFWwindow* window,int key,int scancode,int action,int mods) {
     if (key == GLFW_KEY_I && action == GLFW_PRESS) {
-        programState->ImguiEnable = !programState->ImguiEnable;
-        if (programState->ImguiEnable) {
+        ps.ImguiEnable = !ps.ImguiEnable;
+        if (ps.ImguiEnable) {
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
         } else {
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
@@ -431,11 +278,11 @@ void key_callback(GLFWwindow* window,int key,int scancode,int action,int mods) {
     }
     if(key == GLFW_KEY_F && action == GLFW_PRESS){
         glEnable(GL_MULTISAMPLE);
-        programState->enableAntialiasing = true;
+        ps.enableAntialiasing = true;
     }
     if(key == GLFW_KEY_G && action == GLFW_PRESS){
         glDisable(GL_MULTISAMPLE);
-        programState->enableAntialiasing = false;
+        ps.enableAntialiasing = false;
     }
 
 }
--- src/tools/DataStructs.h
+++ src/tools/DataStructs.h
@@ -6,6 +6,7 @@
 #define CGRAPHICS_DATASTRUCTS_H
 #include <string>
 #include <glm/glm.hpp>
+#include "Camera.h"
 
 struct Vertex {
     glm::vec3 Position;
@@ -17,6 +18,23 @@ struct Texture {
     unsigned id;
     std::string type;
     std::string path;
+};
+
+
+struct ProgramState{
+    bool ImguiEnable= false;
+    void LoadFromDisk(std::string path);
+    Camera camera;
+    glm::vec3 lightColor;
+    glm::vec3 lightColor1;
+    bool EnableMouseMovement = true;
+    float linear = 0.7;
+    float quadratic = 0.032;
+    bool enableAntialiasing = false;
+    ProgramState()
+            :camera(glm::vec3(0.0f,4.0f,35.5f)),lightColor(glm::vec3(1.0f,1.0f,1.0f))
+            ,lightColor1(glm::vec3(1.0f,1.0f,1.0f)){}
+
 };
 
 
